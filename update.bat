dcbab83a41f1cececf60d90473c9903f8b3569e4
Sunny Xu <43919034+therealsunnyxu@users.noreply.github.com>
2024-08-01 13:24:02 -0700

    rearranged files

--- update.bat
+++ update.bat
@@ -2,37 +2,16 @@
 setlocal
 
 REM Step 1: Check if git is in the system path
-echo Checking for git...
 git --version >nul 2>&1
 if %errorlevel% neq 0 (
     echo Git is not installed or not in the system path.
     exit /b 1
 )
 
-echo Checking git...
 REM Step 2: Check if .git exists
 if not exist ".git" (
-    echo .git directory does not exist. Initializing repository and fetching contents.
-    git init
-    if %errorlevel% neq 0 (
-        echo Failed to initialize repository.
-        exit /b 1
-    )
-    git remote add origin https://github.com/therealsunnyxu/charm-health-fax-counter.git
-    if %errorlevel% neq 0 (
-        echo Failed to add remote repository.
-        exit /b 1
-    )
-    git fetch
-    if %errorlevel% neq 0 (
-        echo Failed to fetch repository.
-        exit /b 1
-    )
-    git checkout -t origin/main -f
-    if %errorlevel% neq 0 (
-        echo Failed to checkout main branch.
-        exit /b 1
-    )
+    echo .git directory does not exist.
+	exit /b 1
 ) else (
     echo .git directory exists. Fetching updates.
     git pull
@@ -42,24 +21,32 @@ if not exist ".git" (
     )
 )
 
-echo Checking for virtual environment...
-cd charm-health-fax-counter
-REM Step 3: Check if the virtual environment exists
-if not exist "venv\Scripts\python.exe" (
-    echo Virtual environment not found. Please create it first.
+REM Step 3: Check if python is in the system path
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed or not in the system path.
     exit /b 1
 )
 
-REM Step 4: Activate the virtual environment
-echo Activating virtual environment...
+cd charm-health-fax-counter
+REM Step 4: Create a virtual environment if it does not exist
+if not exist "venv\Scripts\python.exe" (
+    echo Virtual environment not found. Creating virtual environment.
+    python -m venv venv
+    if %errorlevel% neq 0 (
+        echo Failed to create a virtual environment.
+        exit /b 1
+    )
+)
+
+REM Step 5: Activate the virtual environment
 call venv\Scripts\activate
 if %errorlevel% neq 0 (
     echo Failed to activate the virtual environment.
     exit /b 1
 )
 
-REM Step 5: Update the requirements
-echo Installing libraries...
+REM Step 6: Install or update the requirements
 pip install -r requirements.txt
 if %errorlevel% neq 0 (
     echo Failed to install requirements from requirements.txt.
@@ -67,6 +54,6 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-echo Repository and requirements are up to date.
+echo All steps completed successfully.
 deactivate
 exit /b 0
